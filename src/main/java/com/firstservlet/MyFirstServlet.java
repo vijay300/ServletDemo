867f95a50999bf24038676c30f63bd6bd5e0e217
vijay300 <gnvijayk@gmail.com>
2021-01-13 11:47:06 +0530

    [Vijay] Added: new java class and required dependencies and bulid for tomcat

---
+++ src/main/java/com/firstservlet/MyFirstServlet.java
@@ -0,0 +1,20 @@
+package com.firstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3> Hello World My First Servlet</h3>");
+        out.close();
+    }
+}
